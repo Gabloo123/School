3051fb2b55a78d40be37fd55ae2d5e5bb4719da5
Gabriel Korecki <50703570+Gabloo123@users.noreply.github.com>
2019-05-20 18:36:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$(".title").click(function(){
+$(".desc").slideToggle("slow");
+});
+$(".title1").click(function(){
+$(".desc1").slideToggle("slow");
+});
+$(".title2").click(function(){
+$(".desc2").slideToggle("slow");
+});
+$(".title3").click(function(){
+$(".desc3").slideToggle("slow");
+});
+});
+
+
